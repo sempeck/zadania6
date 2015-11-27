d98f894114e5949d04dbc9661f75b3d743756a4c
sempeck <jarek.sepek@mediorama.pl>
2015-11-27 16:16:45 +0100

    Zadanie 2

---
+++ zad2.pas
@@ -0,0 +1,39 @@
+program zad2;
+
+var
+	tablica : array[1..100] of Integer;
+    i,x,y, suma: Integer;
+    tn : Char;
+
+begin
+
+repeat
+	
+  randomize;
+
+  for i := 1 to 100 do
+    begin
+        x:= random(40)+11;
+        y:= random(2);
+
+        if y = 0 then x:=-x;
+
+        tablica[i] :=x;
+
+        if tablica[i] mod 2 = 0 then suma := tablica[i] + suma;
+        
+    end;
+
+    for i:=1 to 100 do 
+      begin
+        writeln(tablica[i]);
+      end;
+     
+	writeln('Suma liczb parzystych to: ', suma);
+	writeln;
+	writeln('Jeszcze raz?');
+	readln(tn); 
+
+until tn = 'n';
+
+end.
